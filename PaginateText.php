c3fc53d86e802ba633eebf137dc9d3f769504c03
Jayprakash12345 <0freerunning@gmail.com>
2018-01-19 11:14:32 +0000

    Convert PaginateText to use extension registration
    Bug: T185305
    Change-Id: Ied098567d995059eec7f42ffde8a6b1609158b3f

--- PaginateText.php
+++ PaginateText.php
@@ -9,46 +9,16 @@
  * @author Ike Hecht, 2014
  * @license GNU General Public Licence 2.0 or later
  */
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'PaginateText',
-	'author' => array(
-		'Ike Hecht',
-	),
-	'version' => '0.1.0',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:PaginateText',
-	'descriptionmsg' => 'paginatetext-desc',
-	'license-name' => 'GPL-2.0+',
-);
-
-/* Setup */
-
-// Register files
-$wgAutoloadClasses['PaginateTextHooks'] = __DIR__ . '/PaginateText.hooks.php';
-$wgMessagesDirs['PaginateText'] = __DIR__ . '/i18n';
-
-// Register hooks
-$wgHooks['ParserFirstCallInit'][] = 'PaginateTextHooks::addTag';
-$wgHooks['BeforePageDisplay'][] = 'PaginateTextHooks::addModules';
-
-// Register modules
-$wgResourceModules['ext.PaginateText'] = array(
-	'scripts' => array(
-		'modules/ext.PaginateText.js',
-	),
-	'styles' => array(
-		'modules/ext.PaginateText.css',
-	),
-	'dependencies' => array(
-		'ext.PaginateText.TextPager'
-	),
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'PaginateText',
-);
-$wgResourceModules['ext.PaginateText.TextPager'] = array(
-	'scripts' => array(
-		'modules/jquery_textpager/jquery.textpager.js',
-	),
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'PaginateText',
-);
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'PaginateText' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['PaginateText'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the PaginateText extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the PaginateText extension requires MediaWiki 1.29+' );
+}
